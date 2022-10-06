51d1fff7ba43935b6b4a8728601c1b6ae4c6f460
persan <per.s.sandberg@bahnhof.se>
2022-10-06 21:47:02 +0200

    Data capture finished.

--- src/ddsarchitectspy-main.adb
+++ src/ddsarchitectspy-main.adb
@@ -7,19 +7,79 @@ with GNAT.Exception_Traces;
 with GNAT.Traceback.Symbolic;
 with DDSArchitectSpy.DDSExtra;
 with RTIDDS.Config;
-
+with GNAT.Formatted_String;
 procedure Ddsarchitectspy.Main is
    use Ada.Text_IO;
    use DDSArchitectSpy.DDSExtra;
 
-   procedure Put_Line (Item : DDS.ParticipantBuiltinTopicData) is
+   function Image (Item : DDS.BuiltinTopicKey_T) return String is
+      use GNAT.Formatted_String;
+      F : constant GNAT.Formatted_String.Formatted_String := +("[%08x, %08x %08x, %08x]");
    begin
-      Put_Line ("Name => """ & Item.Participant_Name.Name.To_Standard_String & """");
-      Put_Line (Item.Key'Img);
+      return -(F & Long_Integer (Item.Value (Item.Value'First))  & Long_Integer (Item.Value (Item.Value'First + 1)) & Long_Integer (Item.Value (Item.Value'First + 2)) & Long_Integer (Item.Value (Item.Value'First + 3)));
+   end;
+
+   function Image (Item : DDS.String) return String is
+      use DDS;
+   begin
+      return '"' & (if Item = DDS.NULL_String then """" else (Item.To_Standard_String & '"'));
+   end;
+
+   function Image (Item : DDS.EntityNameQosPolicy) return String is
+      use DDS;
+   begin
+      return "[" & Image (Item.Name) & ", " & Image (Item.Role_Name) & "]";
+   end;
+
+   procedure Put (Item : DDS.ParticipantBuiltinTopicData) is
+      First_Line : Boolean := True;
+   begin
+      Put_Line ("(Key              => " & Image (Item.Key) & ",");
+      Put_Line (" Participant_Name => " & Image (Item.Participant_Name) & ",");
+      Put      (" Property         => [");
       for P of Item.Property.Value loop
-         Put_Line (P.all.Name.To_Standard_String & "-> " & P.all.Value.To_Standard_String);
+         Put ((if First_Line then """" else "," & ASCII.LF & "                      """));
+         First_Line := False;
+         Put (P.all.Name.To_Standard_String & """=> """ & P.all.Value.To_Standard_String & """");
       end loop;
-   end Put_Line;
+      Put_Line ("],");
+      Put_Line (" Domain_Id        => " & Item.Domain_Id'Img & ")");
+   end Put;
+   procedure Put (Item : DDS.PublicationBuiltinTopicData) is
+      First_Line : Boolean := True;
+   begin
+      Put_Line ("(Key              => " & Image (Item.Key) & ",");
+      Put_Line (" Participant_Key  => " & Image (Item.Participant_Key) & ",");
+      Put_Line (" Publication_Name => " & Image (Item.Publication_Name) & ",");
+      Put_Line (" Topic_Name       => " & Image (Item.Topic_Name) & ",");
+      Put_Line (" Type_Name        =>  " & Image (Item.Type_Name) & ",");
+      Put      (" Property         => [");
+      for P of Item.Property.Value loop
+         Put ((if First_Line then """" else "," & ASCII.LF & "                      """));
+         First_Line := False;
+         Put (P.all.Name.To_Standard_String & """=> """ & P.all.Value.To_Standard_String & """");
+      end loop;
+      Put_Line ("],");
+   end Put;
+
+   procedure Put (Item : DDS.SubscriptionBuiltinTopicData) is
+      First_Line : Boolean := True;
+   begin
+      Put_Line ("(Key              => " & Image (Item.Key) & ",");
+      Put_Line (" Participant_Key  => " & Image (Item.Participant_Key) & ",");
+      Put_Line (" Subscription_Name=> " & Image (Item.Subscription_Name) & ",");
+
+      Put_Line (" Topic_Name       => " & Image (Item.Topic_Name) & ",");
+      Put_Line (" Type_Name        =>  " & Image (Item.Type_Name) & ",");
+      Put      (" Property         => [");
+      for P of Item.Property.Value loop
+         Put ((if First_Line then """" else "," & ASCII.LF & "                      """));
+         First_Line := False;
+         Put (P.all.Name.To_Standard_String & """=> """ & P.all.Value.To_Standard_String & """");
+      end loop;
+      Put_Line ("],");
+   end Put;
+
    E : DDS.Entity.Ref_Access;
 begin
 
@@ -29,24 +89,26 @@ begin
    if Command_Line.Parser.Parse then
       Initialze;
       delay 5.0;
+      Put_Line ("Publications:");
       for I of Publications_DR.Read loop
-         Put_Line (I.Data.Topic_Name.To_Standard_String & " : " & I.Data.Type_Name.To_Standard_String);
-         E := Lookup_Entity (Participant, I.Data.Key);
-
-         Put_Line ((if E /= null then Ada.Tags.External_Tag (E'Tag) else "<NULL>"));
+         if I.Sample_Info.Valid_Data then
+            Put (I.Data.all);
+         end if;
       end loop;
 
-      Put_Line ("------------------------------------------------");
+      Put_Line ("Subscriptions:");
+      for I of Subscriptions_DR.Read loop
+         if I.Sample_Info.Valid_Data then
+            Put (I.Data.all);
+         end if;
+      end loop;
+
+      Put_Line ("Participants:");
       for I of Participants_DR.Read  loop
-         begin
-            Put_Line (I.Data.all);
-         exception
-            when others =>
-               Put_Line ("unable to read");
-         end;
-
+         if I.Sample_Info.Valid_Data then
+            Put (I.Data.all);
+         end if;
       end loop;
-
    end if;
 
 end Ddsarchitectspy.Main;
--- src/ddsarchitectspy.ads
+++ src/ddsarchitectspy.ads
@@ -5,14 +5,14 @@ with DDS.PublicationBuiltinTopicData_DataReader;
 with DDS.SubscriptionBuiltinTopicData_DataReader;
 with DDS.Subscriber;
 package DDSArchitectSpy is
-   factory : constant DDS.DomainParticipantFactory.Ref_Access := DDS.DomainParticipantFactory.Get_Instance;
-   participant : DDS.DomainParticipant.Ref_Access;
-   builtinSubscriber : DDS.Subscriber.Ref_Access;
+   Factory           : constant DDS.DomainParticipantFactory.Ref_Access := DDS.DomainParticipantFactory.Get_Instance;
+   Participant       : DDS.DomainParticipant.Ref_Access;
+   BuiltinSubscriber : DDS.Subscriber.Ref_Access;
 
-   participants_DR    : DDS.ParticipantBuiltinTopicData_DataReader.ref_access;
-   publications_DR    : DDS.PublicationBuiltinTopicData_DataReader.ref_access;
-   subscriptions_DR    : DDS.SubscriptionBuiltinTopicData_DataReader.ref_access;
+   Participants_DR     : DDS.ParticipantBuiltinTopicData_DataReader.Ref_Access;
+   Publications_DR     : DDS.PublicationBuiltinTopicData_DataReader.Ref_Access;
+   Subscriptions_DR    : DDS.SubscriptionBuiltinTopicData_DataReader.Ref_Access;
 
-   procedure initialze;
+   procedure Initialze;
 
 end DDSArchitectSpy;
